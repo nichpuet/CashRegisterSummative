faf9a576cd647531232a90f414ef3a46b30a2bd9
nichpuet <napuetz@gmail.com>
2017-10-05 13:30:51 -0400

    Add project files.

---
+++ CashRegisterSummative/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace CashRegisterSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.burgerInput = new System.Windows.Forms.TextBox();
+            this.fryInput = new System.Windows.Forms.TextBox();
+            this.drinkInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.burgerLabel = new System.Windows.Forms.Label();
+            this.fryLabel = new System.Windows.Forms.Label();
+            this.drinkLabel = new System.Windows.Forms.Label();
+            this.costOutput = new System.Windows.Forms.Label();
+            this.reciptLabel = new System.Windows.Forms.Label();
+            this.recievedInput = new System.Windows.Forms.TextBox();
+            this.recievedLabel = new System.Windows.Forms.Label();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeOutput = new System.Windows.Forms.Label();
+            this.reciptButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // burgerInput
+            // 
+            this.burgerInput.Location = new System.Drawing.Point(72, 24);
+            this.burgerInput.Name = "burgerInput";
+            this.burgerInput.Size = new System.Drawing.Size(100, 20);
+            this.burgerInput.TabIndex = 0;
+            // 
+            // fryInput
+            // 
+            this.fryInput.Location = new System.Drawing.Point(72, 51);
+            this.fryInput.Name = "fryInput";
+            this.fryInput.Size = new System.Drawing.Size(100, 20);
+            this.fryInput.TabIndex = 1;
+            // 
+            // drinkInput
+            // 
+            this.drinkInput.Location = new System.Drawing.Point(72, 77);
+            this.drinkInput.Name = "drinkInput";
+            this.drinkInput.Size = new System.Drawing.Size(100, 20);
+            this.drinkInput.TabIndex = 2;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(72, 104);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(100, 25);
+            this.calculateButton.TabIndex = 3;
+            this.calculateButton.Text = "Calculate Cost";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // burgerLabel
+            // 
+            this.burgerLabel.AutoSize = true;
+            this.burgerLabel.Location = new System.Drawing.Point(-2, 27);
+            this.burgerLabel.Name = "burgerLabel";
+            this.burgerLabel.Size = new System.Drawing.Size(68, 13);
+            this.burgerLabel.TabIndex = 4;
+            this.burgerLabel.Text = "# of Burgers:";
+            // 
+            // fryLabel
+            // 
+            this.fryLabel.AutoSize = true;
+            this.fryLabel.Location = new System.Drawing.Point(12, 54);
+            this.fryLabel.Name = "fryLabel";
+            this.fryLabel.Size = new System.Drawing.Size(54, 13);
+            this.fryLabel.TabIndex = 5;
+            this.fryLabel.Text = "# of Fries:";
+            // 
+            // drinkLabel
+            // 
+            this.drinkLabel.AutoSize = true;
+            this.drinkLabel.Location = new System.Drawing.Point(6, 80);
+            this.drinkLabel.Name = "drinkLabel";
+            this.drinkLabel.Size = new System.Drawing.Size(62, 13);
+            this.drinkLabel.TabIndex = 6;
+            this.drinkLabel.Text = "# of Drinks:";
+            // 
+            // costOutput
+            // 
+            this.costOutput.AutoSize = true;
+            this.costOutput.Location = new System.Drawing.Point(72, 136);
+            this.costOutput.Name = "costOutput";
+            this.costOutput.Size = new System.Drawing.Size(97, 13);
+            this.costOutput.TabIndex = 7;
+            this.costOutput.Text = "Calculate Cost First";
+            // 
+            // reciptLabel
+            // 
+            this.reciptLabel.BackColor = System.Drawing.Color.Snow;
+            this.reciptLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.reciptLabel.Cursor = System.Windows.Forms.Cursors.Default;
+            this.reciptLabel.Location = new System.Drawing.Point(229, 9);
+            this.reciptLabel.Name = "reciptLabel";
+            this.reciptLabel.Size = new System.Drawing.Size(143, 233);
+            this.reciptLabel.TabIndex = 8;
+            this.reciptLabel.Text = "Recipt";
+            this.reciptLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // recievedInput
+            // 
+            this.recievedInput.Location = new System.Drawing.Point(99, 185);
+            this.recievedInput.Name = "recievedInput";
+            this.recievedInput.Size = new System.Drawing.Size(100, 20);
+            this.recievedInput.TabIndex = 9;
+            // 
+            // recievedLabel
+            // 
+            this.recievedLabel.AutoSize = true;
+            this.recievedLabel.Location = new System.Drawing.Point(-2, 188);
+            this.recievedLabel.Name = "recievedLabel";
+            this.recievedLabel.Size = new System.Drawing.Size(95, 13);
+            this.recievedLabel.TabIndex = 10;
+            this.recievedLabel.Text = "Amount Recieved:";
+            // 
+            // changeButton
+            // 
+            this.changeButton.Location = new System.Drawing.Point(72, 211);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(97, 36);
+            this.changeButton.TabIndex = 11;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.AutoSize = true;
+            this.changeOutput.Location = new System.Drawing.Point(69, 250);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(113, 13);
+            this.changeOutput.TabIndex = 12;
+            this.changeOutput.Text = "Calculate Change First";
+            // 
+            // reciptButton
+            // 
+            this.reciptButton.Location = new System.Drawing.Point(229, 245);
+            this.reciptButton.Name = "reciptButton";
+            this.reciptButton.Size = new System.Drawing.Size(143, 23);
+            this.reciptButton.TabIndex = 13;
+            this.reciptButton.Text = "Print Recipt";
+            this.reciptButton.UseVisualStyleBackColor = true;
+            this.reciptButton.Click += new System.EventHandler(this.reciptButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(384, 270);
+            this.Controls.Add(this.reciptButton);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.recievedLabel);
+            this.Controls.Add(this.recievedInput);
+            this.Controls.Add(this.reciptLabel);
+            this.Controls.Add(this.costOutput);
+            this.Controls.Add(this.drinkLabel);
+            this.Controls.Add(this.fryLabel);
+            this.Controls.Add(this.burgerLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.drinkInput);
+            this.Controls.Add(this.fryInput);
+            this.Controls.Add(this.burgerInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox burgerInput;
+        private System.Windows.Forms.TextBox fryInput;
+        private System.Windows.Forms.TextBox drinkInput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label burgerLabel;
+        private System.Windows.Forms.Label fryLabel;
+        private System.Windows.Forms.Label drinkLabel;
+        private System.Windows.Forms.Label costOutput;
+        private System.Windows.Forms.Label reciptLabel;
+        private System.Windows.Forms.TextBox recievedInput;
+        private System.Windows.Forms.Label recievedLabel;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeOutput;
+        private System.Windows.Forms.Button reciptButton;
+    }
+}
+
---
+++ CashRegisterSummative/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CashRegisterSummative
+{
+    public partial class Form1 : Form
+    {
+        const double TAX = 1.13;
+        const double BURGER_PRICE = 2.49;
+        const double FRY_PRICE = 1.89;
+        const double DRINK_PRICE = 0.99;
+        double preTaxCost = 0;
+        double taxCost = 0;
+        double afterTaxCost = 0;
+        double custBill = 0;
+        double custChange = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            double burgerCost;
+            double fryCost;
+            double drinkCost;
+            int burgerOrder;
+            int fryOrder;
+            int drinkOrder;
+            try
+            {
+                burgerOrder = Convert.ToInt16(burgerInput.Text);
+                fryOrder = Convert.ToInt16(fryInput.Text);
+                drinkOrder = Convert.ToInt16(drinkInput.Text);
+
+                burgerCost = burgerOrder * BURGER_PRICE;
+                fryCost = fryOrder * FRY_PRICE;
+                drinkCost = drinkOrder * DRINK_PRICE;
+
+                preTaxCost = burgerCost + fryCost + drinkCost;
+                taxCost = preTaxCost * TAX - preTaxCost;
+                afterTaxCost = preTaxCost * TAX;
+
+                costOutput.Text = "Cost Before Tax: " + preTaxCost.ToString("C") + "\nTax Cost: " + taxCost.ToString("C") + "\nTotal Cost: " + afterTaxCost.ToString("C");
+            }
+            catch
+            {
+                costOutput.Text = "Please enter valid values into the fields";
+            }
+
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            double amountRecieved;
+            double changeDue;
+            try
+            {
+                amountRecieved = Convert.ToInt16(recievedInput.Text);
+                changeDue = amountRecieved - afterTaxCost;
+
+                changeOutput.Text = "Change Due: " + changeDue.ToString("C");
+            }
+            catch
+            {
+                changeOutput.Text = "Input Valid Amount";
+            }
+        }
+
+        private void reciptButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
